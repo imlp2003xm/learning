2089612fd5735b094a3d1a848a5511a9fe5b96f1
imlp2003xm <imlp2003xm@aliyun.com>
2015-11-10 23:57:57 +0800

    Initialize the project.

---
+++ android-dev/CursorLoader/app/src/main/java/com/imlp2003xm/cursorloader/CursorLoaderListFragment.java
@@ -0,0 +1,61 @@
+package com.imlp2003xm.cursorloader;
+
+
+import android.app.ListFragment;
+import android.app.LoaderManager;
+import android.content.Loader;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.SearchView;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CursorLoaderListFragment extends ListFragment
+        implements SearchView.OnQueryTextListener, LoaderManager.LoaderCallbacks<Cursor>{
+
+
+    public CursorLoaderListFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        TextView textView = new TextView(getActivity());
+        textView.setText(R.string.hello_blank_fragment);
+        return textView;
+    }
+
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        return false;
+    }
+}
